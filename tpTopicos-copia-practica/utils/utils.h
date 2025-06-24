40f5ca0b4df0d20f20ec4debf69cd34fa0b51d4d
Carlos Garay <garaycarlosmauricio17@gmail.com>
2025-06-24 19:33:56 -0300

    Remove unused comparators, I/O functions, and data structures
    - Deleted comparators.c and comparators.h as they were no longer needed.
    - Removed io.c and io.h which contained functions for displaying data.
    - Eliminated main.c and main.h, along with associated processing functions and structures.
    - Removed CSV data file indices_icc_general_capitulos.csv as it is no longer referenced.
    - Cleaned up utility functions and their headers, including string manipulation and formatting functions.
    - Deleted variaciones.c and variaciones.h which contained functions for calculating variations.
    - Removed all related structures (Fila and RegistroICC) as they are no longer in use.
    - Cleaned up project configuration file tpTopicos.cbp to reflect the removed files.

--- tpTopicos-copia-practica/utils/utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef UTILS_H_INCLUDED
-#define UTILS_H_INCLUDED
-#include <ctype.h>
-#include <string.h>
-void reemplazarComaPorPunto(char *indice);
-void guionAespacio(char *cadena);
-void primeraMayus(char *cadena);
-void quitarAnteriorAlPrimerGuion(char *cadena);
-#endif // UTILS_H_INCLUDED
